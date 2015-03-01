e2b8ea6c865c672866a3d7b5c9168d757d35144e
Rafael Lopes <rafaellg@vision.ime.usp.br>
2015-03-01 12:08:49 -0300

    Schedule

--- core.py
+++ core.py
@@ -13,3 +13,56 @@
 # along with Aquascapi.  If not, see <http://www.gnu.org/licenses/>.
 # Copyright Rafael Lopes
 
+import config
+import components
+import schedule
+
+
+def schedule_transition(light_channel_component, start_hour, 
+	                    start_poetency, end_potency, transition_duration=15):
+    delta = end_potency - start_poetency
+	for i in range(transition_duration):
+		hour = "%02d:%02d" % (start_hour, i)
+		potency =  start_poetency + (i * (delta / transition_duration))
+		schedule.every().day.at(hour).do(
+			light_channel_component.potency, percentage=potency)
+		)
+
+
+if __name__ == '__main__':
+    light_channel1 = components.LightChannel(config.PINS['light_channel1'])
+    light_channel2 = components.LightChannel(config.PINS['light_channel2'])
+    solenoid_co2 = components.Solenoid(config.PINS['solenoid_co2'])
+    peristaltic_kno3 = components.Peristaltic(config.PINS['peristaltic_kno3'])
+
+    # LIGHT SCHEDULE
+    # first ramp up from 30% to 80% in config.LIGHT_TRANSITION minutes
+    schedule_transition(light_channel_component=light_channel1, 
+    	                start_hour=13, 
+    	                start_poetency=5, end_potency=75)
+    schedule_transition(light_channel_component=light_channel2, 
+    	                start_hour=13,
+    	                start_poetency=5, end_potency=75)
+
+    schedule_transition(light_channel_component=light_channel1, 
+    	                start_hour=16,
+    	                start_poetency=75, end_potency=100)
+    schedule_transition(light_channel_component=light_channel2, 
+    	                start_hour=16,
+    	                start_poetency=75, end_potency=100)
+
+
+
+    schedule_transition(light_channel_component=light_channel1, 
+    	                start_hour=18, 
+    	                start_poetency=100, end_potency=75)
+    schedule_transition(light_channel_component=light_channel2, 
+    	                start_hour=18, 
+    	                start_poetency=100, end_potency=75)
+
+    schedule_transition(light_channel_component=light_channel1, 
+    	                start_hour=21, 
+    	                start_poetency=75, end_potency=0)
+    schedule_transition(light_channel_component=light_channel2, 
+    	                start_hour=21, 
+    	                start_poetency=75, end_potency=0)
