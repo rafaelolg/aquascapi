529b59e886d2a2d9cfb7c757c9cdd6dce41fe5bb
Rafael Lopes <rafaellg@vision.ime.usp.br>
2015-03-01 12:08:38 -0300

    Pin configuration.

---
+++ config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# This file is part of Aquascapi.
+# Aquascapi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# Aquascapi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Aquascapi.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright Rafael Lopes
+
+# Pin Numbers
+LIGHT_TRANSITION = 15
+
+PINS = {'light_channel1': 18,
+        'light_channel2': 19,
+        'solenoid_co2': 5,
+        'peristaltic_kno3': 6,
+        'peristaltic_khpo4': 13,
+        }
